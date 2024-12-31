4a1019d4daeb4198ebc7439422c6b10a8ae15d9a
bvanscoy <vanscoy@wisc.edu>
2024-12-31 17:25:19 -0500

    fix navbar toggler

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,3 +1,12 @@
+function toggleVisibility(id) {
+  var x = document.getElementById(id);
+  if (x.style.display == 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
+
 function hideId(id) {
   var element = document.getElementById(id);
   if (element) {
