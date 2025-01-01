44c3ea90a59432165210081899f879861a9a3914
bvanscoy <vanscoy@wisc.edu>
2025-01-01 12:09:45 -0500

    updated icons

---
+++ static/images/icon/icon.jl
@@ -0,0 +1,50 @@
+using Luxor
+using MathTeXEngine
+
+sz = 75
+
+function icon(px, filename, maskable=false)
+
+    sz = maskable ? 1.5px : px
+
+    Drawing(sz, sz, filename)
+    
+    origin()
+    # background("grey6")
+    background(0,0,0,0)
+    sethue("#236076")
+    a = px/75
+    scale(a)
+    setline(8a)
+
+    p1 = Point(-20, -10)
+    p2 = Point(0, -30)
+    p3 = Point(20, -10)
+    p4 = Point(0, 10)
+    p5 = Point(-35, -25)
+
+    poly([p1, p2, p3, p4, p5], :stroke)
+
+    p6 = Point(0, 30)
+    p7 = Point(-30, 0)
+    p8 = Point(30, 0)
+
+    poly([p7, p6, p8], :stroke)
+
+    # minimum safe area for masking
+    # sethue("red")
+    # setline(1)
+    # circle(Point(0,0), 0.4*sz/a, action=:stroke)
+
+    finish()
+end
+
+icon(100,  "static/images/icon-100.png")
+icon(144,  "static/images/icon-144.png")
+icon(256,  "static/images/icon-256.png")
+icon(512,  "static/images/icon-512.png")
+icon(1024, "static/images/icon-1024.png")
+icon(75,   "static/images/icon.svg")
+icon(75,   "static/images/icon-maskable.svg", true)
+
+preview()
